7d32168e52292ea14a8fa0b20b2f499a9f7dff04
luka.patrun <luka.patrun@gmail.com>
2020-09-30 11:09:48 +0200

    Updates

---
+++ precache-manifest.f292f042209131c55e021e17b49c4e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd01691d610d187a6ff08117bc4fa99c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45f842a5b80ecf1381dc",
+    "url": "/static/css/main.716615f8.chunk.css"
+  },
+  {
+    "revision": "e742f302773134241a2c",
+    "url": "/static/js/2.d7aad8e9.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.d7aad8e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f842a5b80ecf1381dc",
+    "url": "/static/js/main.060c301b.chunk.js"
+  },
+  {
+    "revision": "7455c6a5b2f9a257e905",
+    "url": "/static/js/runtime-main.e85385e6.js"
+  }
+]);
